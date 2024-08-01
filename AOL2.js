cf8cc3583015fc646b64bfeaa2c55825dbbbbb12
Jordan C Cox <166924526+jordancox7483@users.noreply.github.com>
2024-08-01 17:43:04 -0400

    Add files via upload

---
+++ AOL2.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Remove AOL Mail Section 2
+// @namespace    https://mail.aol.com/
+// @version      1.0
+// @description  Remove specific section from AOL Mail
+// @author       Your Name
+// @match        https://mail.aol.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to remove the specified section
+    function removeSection() {
+        const sectionSelector = '#mail-app-component-container > div.D_F.o_v.p_R.I_T';
+        const sectionElement = document.querySelector(sectionSelector);
+        if (sectionElement) {
+            sectionElement.remove();
+        }
+    }
+
+    // Run the function when the page loads
+    window.addEventListener('load', removeSection);
+})();
